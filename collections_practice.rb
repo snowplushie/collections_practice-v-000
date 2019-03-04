9e28283a0dfe0555f0f232a8a5274edc13a8bc5e
Paige Wiener <snowplushie@yahoo.com>
2019-03-04 08:58:01 +0000

    Automatically backed up by Learn

--- collections_practice.rb
+++ collections_practice.rb
@@ -14,7 +14,7 @@ end
 end
 
 def swap_elements(array)
-  array[0], array[1], array[2] == array[0], array[2], array[1]
+  array[0], array[1], array[2] = array[0], array[2], array[1]
   array
 end
 end
